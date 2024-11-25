633e398f97265e787914a78c033701b194f4a69e
shuami-dev <shuami.dev@gmail.com>
2024-11-26 00:06:39 +0800

    Update error status

--- src/errorStatus.ts
+++ src/errorStatus.ts
@@ -4,7 +4,7 @@ import { computed, Ref } from "vue"
  * Type for the custom error handler. Handler comes from user
  */
 export type ErrorHandler = (
-	error: Error | string | null
+	error: Error | string | null | undefined
 ) => string | null
 
 /**
@@ -16,7 +16,7 @@ export type ErrorHandler = (
  * @returns Computed Ref for error status
  */
 export function errorStatus(
-	error: Ref<Error | string | null>,
+	error: Ref<Error | string | null | undefined>,
 	t: (key: string) => string,
 	errorHandler?: ErrorHandler
 ) {
@@ -29,62 +29,100 @@ export function errorStatus(
 
 			// Handle specific error cases
 			if (error) {
-				if (typeof error.value === "string") {
-					if (error.value.includes("ETIMEDOUT")) {
-						return t("errStatus.connectionTimedOut")
-					} else if (error.value.includes("ECONNREFUSED")) {
-						return t("errStatus.connectionRefused")
-					} else if (error.value.includes("EREQERROR")) {
-						return t("errStatus.requestErr")
-					} else if (error.value.includes("403")) {
-						return t("errStatus.403")
-					} else if (error.value.includes("404")) {
-						return t("errStatus.404")
-					} else if (error.value.includes("500")) {
-						return t("errStatus.500")
-					} else if (error.value.includes("502")) {
-						return t("errStatus.502")
-					} else if (error.value.includes("503")) {
-						return t("errStatus.503")
-					} else if (error.value.includes("504")) {
-						return t("errStatus.504")
-					} else {
-						if (errorHandler) {
-							const customMessage = errorHandler(error.value)
-							if (customMessage) return customMessage
-						} else {
-							return t("errStatus.defaultStatusError")
-						}
-					}
-				} else if (error.value instanceof Error) {
-					if (error.value.message.includes("ETIMEDOUT")) {
-						return t("errStatus.connectionTimedOut")
-					} else if (error.value.message.includes("ECONNREFUSED")) {
-						return t("errStatus.connectionRefused")
-					} else if (error.value.message.includes("EREQERROR")) {
-						return t("errStatus.requestErr")
-					} else if (error.value.message.includes("403")) {
-						return t("errStatus.403")
-					} else if (error.value.message.includes("404")) {
-						return t("errStatus.404")
-					} else if (error.value.message.includes("500")) {
-						return t("errStatus.500")
-					} else if (error.value.message.includes("502")) {
-						return t("errStatus.502")
-					} else if (error.value.message.includes("503")) {
-						return t("errStatus.503")
-					} else if (error.value.message.includes("504")) {
-						return t("errStatus.504")
-					} else {
-						if (errorHandler) {
-							const customMessage = errorHandler(error.value)
-							if (customMessage) return customMessage
-						} else {
-							return t("errStatus.defaultStatusError")
-						}
-					}
+				if (
+					(typeof error.value === "string" &&
+						error.value.includes("NORESPONSE")) ||
+					(error.value instanceof Error &&
+						error.value.message.includes("NORESPONSE"))
+				) {
+					return t("errStatus.noResponse")
+				} else if (
+					(typeof error.value === "string" &&
+						error.value.includes("ETIMEDOUT")) ||
+					(error.value instanceof Error &&
+						error.value.message.includes("ETIMEDOUT"))
+				) {
+					return t("errStatus.connectionTimedOut")
+				} else if (
+					(typeof error.value === "string" &&
+						error.value.includes("ECONNREFUSED")) ||
+					(error.value instanceof Error &&
+						error.value.message.includes("ECONNREFUSED"))
+				) {
+					return t("errStatus.connectionRefused")
+				} else if (
+					(typeof error.value === "string" &&
+						error.value.includes("EREQERROR")) ||
+					(error.value instanceof Error &&
+						error.value.message.includes("EREQERROR"))
+				) {
+					return t("errStatus.requestErr")
+				} else if (
+					(typeof error.value === "string" &&
+						error.value.includes("ECONNABORTED")) ||
+					(error.value instanceof Error &&
+						error.value.message.includes("ECONNABORTED"))
+				) {
+					return t("errStatus.connAbort")
+				} else if (
+					(typeof error.value === "string" &&
+						error.value.includes("ENOTFOUND")) ||
+					(error.value instanceof Error &&
+						error.value.message.includes("ENOTFOUND"))
+				) {
+					return t("errStatus.notFound")
+				} else if (
+					(typeof error.value === "string" && error.value.includes("400")) ||
+					(error.value instanceof Error && error.value.message.includes("400"))
+				) {
+					return t("errStatus.400")
+				} else if (
+					(typeof error.value === "string" && error.value.includes("401")) ||
+					(error.value instanceof Error && error.value.message.includes("401"))
+				) {
+					return t("errStatus.401")
+				} else if (
+					(typeof error.value === "string" && error.value.includes("403")) ||
+					(error.value instanceof Error && error.value.message.includes("403"))
+				) {
+					return t("errStatus.403")
+				} else if (
+					(typeof error.value === "string" && error.value.includes("404")) ||
+					(error.value instanceof Error && error.value.message.includes("404"))
+				) {
+					return t("errStatus.404")
+				} else if (
+					(typeof error.value === "string" && error.value.includes("429")) ||
+					(error.value instanceof Error && error.value.message.includes("429"))
+				) {
+					return t("errStatus.429")
+				} else if (
+					(typeof error.value === "string" && error.value.includes("500")) ||
+					(error.value instanceof Error && error.value.message.includes("500"))
+				) {
+					return t("errStatus.500")
+				} else if (
+					(typeof error.value === "string" && error.value.includes("502")) ||
+					(error.value instanceof Error && error.value.message.includes("502"))
+				) {
+					return t("errStatus.502")
+				} else if (
+					(typeof error.value === "string" && error.value.includes("503")) ||
+					(error.value instanceof Error && error.value.message.includes("503"))
+				) {
+					return t("errStatus.503")
+				} else if (
+					(typeof error.value === "string" && error.value.includes("504")) ||
+					(error.value instanceof Error && error.value.message.includes("504"))
+				) {
+					return t("errStatus.504")
 				} else {
-					return t("errStatus.defaultStatusError")
+					if (errorHandler) {
+						const customMessage = errorHandler(error.value)
+						if (customMessage) return customMessage
+					} else {
+						return t("errStatus.defaultStatusError")
+					}
 				}
 			} else {
 				return t("errStatus.defaultStatusError")
